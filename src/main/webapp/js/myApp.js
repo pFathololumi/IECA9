dd154f48c141a898f495f2b0064d9972194127e5
pFathololumi <p16_f3@yahoo.com>
2016-05-23 14:46:59 +0430

    create sign in and sing up page...

---
+++ src/main/webapp/js/myApp.js
@@ -0,0 +1,21 @@
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+(function () {
+    var app = angular.module('myApp', ['UserValidation']);
+
+angular.module('UserValidation', []).directive('validPasswordC', function () {
+    return {
+        require: 'ngModel',
+        link: function (scope, elm, attrs, ctrl) {
+            ctrl.$parsers.unshift(function (viewValue, $scope) {
+                var noMatch = viewValue !== scope.myForm.password.$viewValue;
+                ctrl.$setValidity('noMatch', !noMatch);
+            });
+        }
+    };
+});
+})();
